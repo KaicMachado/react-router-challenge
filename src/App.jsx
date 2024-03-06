db6a5c66b1c642b8997136786a88f5a3d0ce866b
Kaic Machado <kaicmachado2@gmail.com>
2024-03-05 22:24:29 -0300

    corrigindo bug de header pulando a cada navegação

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import Produtos from "./Components/Produtos";
 import Contato from "./Components/Contato";
 import Produto from "./Components/Produto";
 import Footer from "./Components/Footer";
+import styled from "styled-components";
+
+const ContentApp = styled.div`
+ display: flex;
+ flex-direction: column;
+ max-width: 45rem;
+ margin: 0 auto;
+ min-height: calc(100vh + 5rem);
+`;
 
 function App() {
  return (
-  <div>
+  <ContentApp>
    <BrowserRouter>
     <Routes>
      <Route path="/" element={<Produtos />} />
@@ -15,7 +24,7 @@ function App() {
     </Routes>
    </BrowserRouter>
    <Footer />
-  </div>
+  </ContentApp>
  );
 }
 
